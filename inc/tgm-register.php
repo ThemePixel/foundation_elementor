1ddd92869cacb967560936fc26470ba0cc8eca64
z9110858@gmail.com <z9110858@gmail.com>
2023-01-26 10:33:19 +0700

    footer/header/assets/-html done.start.

---
+++ inc/tgm-register.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+
+
+add_action( 'tgmpa_register', 'qwetheme_register_required_plugins' );
+
+
+function qwetheme_register_required_plugins() {
+
+	$plugins = array(
+
+	
+		//array(
+			/*'name'               => 'TGM Example Plugin', // The plugin name.
+			'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),*/
+
+	
+
+	
+	
+		array(
+			'name'      => 'Template Library and Redux Framework',
+			'slug'      => 'redux-framework',
+			'required'  => false,
+		),
+	    array(
+			'name'      => 'MC4WP: Mailchimp for WordPress',
+			'slug'      => 'mailchimp-for-wp',
+			'required'  => false,
+		),
+		);
+
+
+	$config = array(
+		'id'           => 'qwetheme',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+
+	);
+
+	tgmpa( $plugins, $config );
+}
